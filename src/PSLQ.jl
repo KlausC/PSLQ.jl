05b7428046249bdf165ffa4c925ac2794aa8c893
Klaus Crusius <klaus.crusius@web.de>
2019-07-06 18:40:25 +0200

    refactoring

--- src/PSLQ.jl
+++ src/PSLQ.jl
@@ -2,67 +2,122 @@ module PSLQ
 
 using LinearAlgebra
 
-function pslq_step(H, A, B)
-    n = size(H, 1)
-    D = make_D(H)
-    E = make_E(D)
-    H = D * H
+function pslq_step(H::Matrix{T}, B::Matrix{Ti}) where {T<:Real,Ti<:Integer}
+    pslq_step!(copy(H), copy(B), UnitLowerTriangular(similar(B)))
+end
+function pslq_step!(H::Matrix{T}, B::Matrix{Ti},
+                    D::UnitLowerTriangular{Ti,Matrix{Ti}}) where {T<:Real,Ti<:Integer}
+    n = check_H(H)
+    check_square(D, n)
+    check_square(B, n)
+    make_D!(D, H, nint)
+    lmul!(D, H)
     γ = 2.0
-    δ = sqrt(3/4 - 1/γ^2)
-    j = findmax(abs.(diag(H) .* γ .^ (1:n-1)))[2]
-    Rj = make_R(H, j)
-    Gj = make_G(H, j)
-    H = Rj * H * Gj
-    if j < n-1; H[j,j+1] = 0; end
-    A = Rj * D * A
-    B = B * E * Rj
-    H, A, B
+    j = findmaxj(H, γ)
+    exrows!(H, j, j+1, 1:j)
+    givenscol!(H, j)
+    rdiv!(B, D) 
+    excols!(B, j, j+1, 1:n)
+    H, B
 end
 
-function make_D(H::Array{<:Real,2})
-    nint(x) = Integer(round(x))
-    Ti = typeof(Integer(eltype(H)(0)))
+function findmaxj(H::Matrix, γ::Real)
+    # δ = sqrt(3/4 - 1/γ^2) must be > 0
+    # j = findmax(abs.(diag(H) .* γ .^ (1:n-1)))[2]
+    n = min(size(H)...)
+    j = 1
+    msf = abs(H[1,1]) / γ
+    for k = 2:n
+        hkk = abs(H[k,k])
+        if hkk >= msf
+            j, msf = k, hkk
+        end
+        msf /= γ
+    end
+    j
+end
+function check_H(H::AbstractMatrix)
     n = size(H, 1)
-    D = zeros(Ti, n, n)
-    for i = 1:n; D[i,i] = Ti(1); end
-    for i = n:-1:1;
+    if size(H, 2) != n - 1 || n < 2
+        throw(DimensionMismatch("Matrix needs to be nx(n-1) with n >= 2 " *
+                                "but has size $(size(H))"))
+    end
+    n
+end
+function check_square(B::AbstractMatrix, n::Integer)
+    n = size(B, 1)
+    if size(B, 2) != n
+        throw(DimensionMismatch("Matrix needs to be square with size $n but is $(size(B))"))
+    end
+    n
+end
+nint(x) = Integer(round(x))
+integertype(::Type{T}) where T = typeof(Integer(T(0)))
+integertype(::Type{BigFloat}) = BigInt
+function make_D(H::Matrix{T}, nint::Function=nint) where {T<:Real}
+    n = size(H, 1)
+    D = Matrix{integertype(T)}(undef, n, n)
+    make_D!(UnitLowerTriangular(D), H, nint)
+end
+function make_D!(DU::UnitLowerTriangular, H::Matrix, nint::Function)
+    n = check_H(H)
+    D = DU.data
+    for i = 2:n
         for j = i-1:-1:1
-            sum = eltype(H)(0)
-            for k = j:i
+            sum = H[i,j]
+            for k = j+1:i-1
                 sum += D[i,k] * H[k,j]
             end
-            D[i,j] = nint(-sum/H[j,j])
+            D[i,j] = nint(-sum / H[j,j])
         end
     end
-    D
+    DU
 end
 
- function make_E(D::Array{<:Integer,2})
-    E = zeros(eltype(D), size(D))
-    n = size(D, 1)
-    for i = 1:n; E[i,i] = 1; end
-    for j = n:-1:1
-        for i = j+1:n
-            sum = eltype(D)(0)
-            for k = j:i
-                sum += E[i,k] * D[k,j]
-            end
-            E[i,j] = -sum
-        end
-    end
-    E
-end
-
-function make_H(x::Vector{<:Real})
-    x = x ./ norm(x)
+function make_H(x::Vector{T}) where T<:Real
     n = length(x)
-    s = sqrt.(reverse(cumsum(reverse(x.^2))))
-    H = zeros(eltype(x), n, n-1)
-    for i = 1:n-1
-        H[i,i] = s[i+1] / s[i]
+    H = zeros(T, n, n-1)
+    b = abs(x[n])
+    a = b^2
+    for j = n-1:-1:1
+        a += x[j]^2
+        c = b
+        b = sqrt(a)
+        H[j,j] = c / b
+        d = -x[j] / (c * b)
+        for i = j+1:n
+            H[i,j] = x[i] * d
+        end
     end
-    for i = 1:n, j = 1:i-1
-        H[i,j] = -x[i]*x[j]/s[j]/s[j+1]
+    H
+end
+
+function givenscol!(H::Matrix{T}, j::Integer) where T<:Real
+    j >= size(H, 2) && return H
+    n = size(H, 1)
+    a, b, c = H[j,j], H[j+1,j+1], H[j+1,j]
+    d = hypot(a, b)
+    a /= d
+    b /= d
+    H[j,j] = d
+    H[j+1,j], H[j+1,j+1] = c * a, -c * b
+    for k = j+2:n
+        u, v = H[k,j], H[k,j+1]
+        H[k,j], H[k,j+1] = u*a + v*b, -u*b + v*a
+    end
+    H
+end
+
+function exrows!(H::Matrix, i1::Integer, i2::Integer, r::UnitRange)
+    for k in r
+        H[i1,k], H[i2,k] = H[i2,k], H[i1,k]
+    end
+    H
+end
+
+function excols!(H::Matrix, i1::Integer, i2::Integer, r::UnitRange)
+    for k in r
+        H[k,i1], H[k,i2] = H[k,i2], H[k,i1]
     end
     H
 end
--- test/pslq.jl
+++ test/pslq.jl
@@ -11,19 +11,29 @@ function example_1(a, n::Integer)
     x
 end
 
-function make_Htest(n::Integer, T=Float64)
+function make_Htest(n::Integer; gamma=1, T=Float64)
     H = zeros(T, n, n-1)
-    for i = 1:n-1; H[i,i] = 1; end
+    for i = 1:n-1; H[i,i] = gamma^(1-i); end
     for i = 1:n, j = 1:i-1
         H[i,j] = i + j*j
     end
     H
 end
 
-import PSLQ: make_H, make_D, make_E, make_G, make_R, pslq_step
+import PSLQ: make_H, make_D, pslq_step, check_H, check_square, findmaxj
 
 @testset "correctness of calculations" begin
-    
+   
+    H = make_Htest(1)
+    @test_throws DimensionMismatch check_H(H)
+
+    H = make_Htest(10, gamma=2.0)
+    @test findmaxj(H, 2.0) == 9
+    H[1,1] = nextfloat(H[1,1])
+    @test findmaxj(H, 2.0) == 1
+    H[7,7] = nextfloat(H[7,7])
+    @test findmaxj(H, 2.0) == 7
+
     H = make_Htest(10)
     D = make_D(H)
     @test norm(D * H - Matrix(I, size(H))) == 0
@@ -34,16 +44,14 @@ import PSLQ: make_H, make_D, make_E, make_G, make_R, pslq_step
     @test norm(H * H' - P) <= 2*eps()*sqrt(size(H,2))
    
     D = make_D(H)
-    E = make_E(D)
-    @test norm(D * E - I) == 0
+    @test D \ D == I
 
-    A1 = Matrix(I, size(H, 1), size(H,1))
-    B1 = copy(A1)
+    B1 = Matrix(I*1, size(H, 1), size(H,1))
     H1 = H
-    H1, A1, B1 = pslq_step(H1, A1, B1)
-    H1, A1, B1 = pslq_step(H1, A1, B1)
-    H1, A1, B1 = pslq_step(H1, A1, B1)
-    H1, A1, B1 = pslq_step(H1, A1, B1)
+    H1, B1 = pslq_step(H1, B1)
+    H1, B1 = pslq_step(H1, B1)
+    H1, B1 = pslq_step(H1, B1)
+    H1, B1 = pslq_step(H1, B1)
     X = H \ (B1 * H1) # verify that H = B1*H1 * X with orthogonal X
     @test norm(X'X - I) <= 10*eps()*size(H,2)
 end
